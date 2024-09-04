1d734fcaaa2930e54c2a8f633ad46d056a55faf6
Rei Reizo <nathan.irwin2021@gmail.com>
2024-09-04 14:54:27 -0400

    Objects and Cube Scripts

---
+++ Assets/Edittor/CubeBehaviorEditor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CubeBehavior))]
+public class CubeBehaviorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+    }
+}
---
+++ Assets/SphereBehavior.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereBehavior : MonoBehaviour
+{
+//No Object Behavior
+}
