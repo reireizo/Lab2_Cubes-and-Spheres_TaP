6cc0b6bc5a0fdaa0c455e24ca1b246342f51aae1
Rei Reizo <nathan.irwin2021@gmail.com>
2024-09-04 15:15:13 -0400

    Cube Script Progress

--- Assets/Edittor/CubeBehaviorEditor.cs
+++ Assets/Edittor/CubeBehaviorEditor.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 using UnityEditor;
 using System.Linq;
 
-[CustomEditor(typeof(CubeBehavior))]
+[CustomEditor(typeof(CubeBehavior)), CanEditMultipleObjects]
 public class CubeBehaviorEditor : Editor
 {
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
+
+        EditorGUILayout.BeginHorizontal();
+
         if (GUILayout.Button("Select All Cubes"))
         {
             var allCubeBehavior = GameObject.FindObjectsOfType<CubeBehavior>();
             var allCubeGameObjects = allCubeBehavior.Select(cube => cube.gameObject).ToArray();
             Selection.objects = allCubeGameObjects;
         }
+        if (GUILayout.Button("ClearSelection"))
+        {
+            Selection.objects = new Object[] { (target as CubeBehavior).gameObject };
+        }
+
+        EditorGUILayout.EndHorizontal();
 
     }
 }
